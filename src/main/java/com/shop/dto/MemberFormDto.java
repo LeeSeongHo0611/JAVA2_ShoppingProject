73322d7c7d6b3ee6653af60be99aa4ddd20ccc40
ho611611 <ho611611@hanmail.net>
2024-07-05 13:22:06 +0900

    2024 07 05 PM 1320
    custom.css src\main\resources\static\css
    // css 스타일 변경, 추가를 위한 요소 파일 추가
    //색깔, 정렬 , 텍스트박스 좌우길이 등
    
    googlelogin.png src\main\resources\static\img
    // 구글로그인 이미지파일
    
    header.html src\main\resources\templates\fragments
    //검색창 늘어나는 스크립트 추가
    
    kakaologin.png src\main\resources\static\img
    // 카카오로그인 이미지파일
    
    layout1.css src\main\resources\static\css
    //변경없음
    
    layout1.html src\main\resources\templates\layouts
    //19-20줄 사용자정의 css 경로 추가
    
    memberForm.html src\main\resources\templates\member
    //변경된 css에 맞춰 수정
    
    MemberFormDto.java src\main\java\com\shop\dto
    //회원가입 이름, 비밀번호, 전화번호 형식 유효성검사 추가
    
    memberLoginForm.html src\main\resources\templates\member
    //변경된 css에 맞춰수정, 소셜로그인 이미지파일 추가
    
    naverlogin.png src\main\resources\static\img
    //네이버 로그인 이미지파일
    
    돋보기.png src\main\resources\static\img
    //돋보기파일 수정하던이미지 미완성
    
    업데이트

--- src/main/java/com/shop/dto/MemberFormDto.java
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -3,24 +3,31 @@ package com.shop.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Getter
 @Setter
 public class MemberFormDto {
     @NotBlank(message = "이름은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "이름에는 특수문자나 숫자를 포함할 수 없습니다.")
     private String name;
+
     @NotEmpty(message = "이메일은 필수 입력 값입니다.")
     @Email
     private String email;
+
     @NotEmpty(message = "비밀번호은 필수 입력 값입니다.")
     @Length(min = 8, max = 16, message = "비밀번호는 8자이상, 16자 이하로 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,16}$", message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요. 또한, 적어도 하나의 특수문자를 포함해야 합니다.")
     private String password;
+
     @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
+
     @NotBlank(message = "전화번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "^[0-9]{9,11}$", message = "전화번호는 9~11자리의 숫자만 입력 가능합니다.")
     private String tel;
 }
